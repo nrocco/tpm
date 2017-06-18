dc83910751cbcb01427b9042b1ef6a350e3c8af9
Nico Di Rocco <dirocco.nico@gmail.com>
2017-06-18 11:10:56 +0200

    Added a basic zsh completion

--- Makefile
+++ Makefile
@@ -8,6 +8,8 @@ tpm: $(GO_FILES)
 install: tpm
 	mkdir -p $(DESTDIR)$(PREFIX)/bin
 	cp $< $(DESTDIR)$(PREFIX)/bin/tpm
+	cp completion.zsh $(DESTDIR)$(PREFIX)/share/zsh/site-functions/_tpm
 
 uninstall:
 	rm -f $(DESTDIR)$(PREFIX)/bin/tpm
+	rm -f $(DESTDIR)$(PREFIX)/share/zsh/site-functions/_tpm
---
+++ completion.zsh
@@ -0,0 +1,28 @@
+#compdef tpm
+_tpm() {
+  local -a commands
+  commands=(
+    'help:Help about any command'
+    'password:Manage passwords'
+    'version:Show version of the client and server'
+  )
+
+  local -a password_commands
+  password_commands=(
+    'help:Help about any command'
+    'show:Show a single password'
+    'search:Search for passwords'
+  )
+
+  if (( CURRENT == 2 )); then
+    _describe -t commands 'commands' commands
+  elif (( CURRENT == 3)); then
+    if [[ $words[2] == 'password' ]]; then
+        _describe -t password_commands 'password_commands' password_commands
+    fi
+  fi
+
+  return 0
+}
+
+_tpm
